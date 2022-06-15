89cbbf353bc44e86e3e43ee7fe0e1cbe83522791
dachava <diegocb1797@gmail.com>
2022-06-14 21:48:02 -0600

    Fixed AddGuitar Fragment

--- app/src/main/java/com/example/guitars/ui/guitar/AddGuitarFragment.kt
+++ app/src/main/java/com/example/guitars/ui/guitar/AddGuitarFragment.kt
@@ -5,21 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.example.guitars.R
 import com.example.guitars.databinding.FragmentAddGuitarBinding
 import com.example.guitars.databinding.FragmentGuitarBinding
+import com.example.guitars.model.Guitar
+import com.example.guitars.viewmodel.GuitarViewModel
 
 
 class AddGuitarFragment : Fragment() {
     private var _binding: FragmentAddGuitarBinding? = null
     private val binding get() = _binding!!
+    private lateinit var guitarViewModel: GuitarViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentAddGuitarBinding.inflate(inflater, container, false)
+        _binding = FragmentAddGuitarBinding.inflate(layoutInflater, container, false)
+        guitarViewModel = ViewModelProvider(this).get(GuitarViewModel::class.java)
+        binding.btAgregar.setOnClickListener{ insertaGuitar()}
 
         return binding.root
+    }
+
+    private fun insertaGuitar(){
+        val marca = binding.etMarca.text.toString()
+        val modelo = binding.etModelo.text.toString()
+        val precio = binding.etPrecio.text.toString()
+        val correo = binding.etCorreo.text.toString()
+        val telefono = binding.etTelefono.text.toString()
+
+        if(validos(marca, modelo, precio, correo, telefono)){
+            val guitar = Guitar(0, marca, modelo, precio, correo, telefono, "", "")
+            guitarViewModel.addGuitar(guitar)
+            Toast.makeText(requireContext(), getString(R.string.msgGuitarAgregado),Toast.LENGTH_LONG).show()
+        } else{
+            Toast.makeText(requireContext(), getString(R.string.msgFaltanDatos),Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun validos(marca: String, modelo: String, precio: String, correo:String, telefono:String): Boolean{
+        return !(marca.isEmpty()|| modelo.isEmpty()|| precio.isEmpty()|| correo.isEmpty()|| telefono.isEmpty())
     }
 
     companion object {
